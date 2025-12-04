89a4d8455e87e33c770fcb570fcb96f58208f538
Jan Frost <mail@janfrost.de>
2025-12-04 19:55:24 +0100

    day3 star 2

--- test/aoc2025/day3_test.clj
+++ test/aoc2025/day3_test.clj
@@ -38,69 +38,56 @@
      (= output (string-to-digits input))
       "999" [9 9 9]))
   
-  (defn digits-with-index [s] (map list (range (- (count s) 1)) (string-to-digits s)))
+  (defn digits-with-index [n s] (map list (range (- (count s) n)) (string-to-digits s)))
   
   (testing "range index"
     (are
-     [input output]
-     (= output (digits-with-index input))
-      "999" ('(0 9) '(1 9))))
+     [input n output]
+     (= output (digits-with-index n input))
+      "999" 1 '((0 9) (1 9))
+      "999" 2 '((0 9))))
   
-  (defn first-index-with-digit [s]
+  (defn largest-digit-with-index [n s]
     (->> s
-         (digits-with-index)
+         (digits-with-index n)
          (reduce #(if (>= (second %1) (second %2)) %1 %2) '(-1 -1))))
   
   (testing "largest number index"
     (are
      [input output]
-     (= output (first-index-with-digit input))
+     (= output (largest-digit-with-index 1 input))
       "987" [0 9]
       "897" [1 9]
       "789" [1 8]
       "999" [0 9]
       "4644333464433434855275434364342435544972433324444444432243354936362863356444434344443333433425344432" [37 9]))
   
-  (defn next-largest-number [index s]
-    (reduce max -1 (string-to-digits (subs s (inc index)))))
+  (defn largest-number [s n]
+    (let [next-digit-with-index (largest-digit-with-index n s)
+          next-substring (subs s (inc (first next-digit-with-index)))]
+      (case n
+        0 (second next-digit-with-index)
+        (+ (* (second next-digit-with-index) (long (Math/pow 10 n)))
+           (largest-number next-substring (dec n))))))
+    
   
-  (testing "second largest number index"
-    (are
-     [input output]
-     (= output (let [largest (first-index-with-digit input)]
-                 (next-largest-number (first largest) input)))
-      "987" 8
-      "897" 7
-      "789" 9
-      "4644333464433434855275434364342435544972433324444444432243354936362863356444434344443333433425344432" 9))
+  (testing "find largest number in string"
+         (are
+          [input output]
+          (= output (largest-number input 11))
+           "987654321111111" 987654321111))
   
-  (defn largest-number [s]
-    (let [largest-with-index (first-index-with-digit s)
-          first-digit (second largest-with-index)
-          second-digit (next-largest-number (first largest-with-index) s)]
-      (+ (* first-digit 10) second-digit)))
-  
-  (testing "largest number below 100"
-    (are
-     [input output]
-     (= output (largest-number input))
-      "873256218561" 88
-      "4644333464433434855275434364342435544972433324444444432243354936362863356444434344443333433425344432" 99)
-    )
-  
-  (testing "day3 star 1"
+  (testing "day3 star 2"
     (are
      [input output]
      (= output (->> input
                     (split-lines)
-                    (map largest-number)
+                    (map #(largest-number % 11))
                     (reduce +)))
       "987654321111111
   811111111111119
   234234234234278
-  818181911112111" 357
-      (slurp "test/aoc2025/day3_input.txt") 17107))
-  
-  
+  818181911112111" 3121910778619
+      (slurp "test/aoc2025/day3_input.txt") 169349762274117)) 
   
   )
